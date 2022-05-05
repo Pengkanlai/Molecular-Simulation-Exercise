8ff3aefef8b5adc78cbffb720e83aff0863bd4de
Kanlai Peng <kanlai.peng@epfl.ch>
2022-05-05 18:45:59 +0200

    answer

--- block3/1-BarrierCrossing_new/Source/integrate_nve.c
+++ block3/1-BarrierCrossing_new/Source/integrate_nve.c
@@ -22,6 +22,13 @@ void IntegrateNVE(void)
   double U,F,NewVelocity;
 
   // start modification
-
+  F= Force(Position,U,F);
+  NewVelocity=Velocity+Tstep*F;
+  Position = Position + NewVelocity*Tstep;
+  //Conserved quantity is v(t)=(v_pos + v_new)/2.
+  //We square them, so we need to factor of 8.
+  ConservedEnergy=U+SQR(NewVelocity+Velocity)/8.0;
+  Velocity=NewVelocity;
+  OldF=F;
   // end modification
 } 
